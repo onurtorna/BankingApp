365f24a61b38e50c8f7aa931e25f7fad8e984130
Onur Torna <torna@nextbike.com>
2019-11-30 00:43:33 +0100

    Add project and folder structure

---
+++ BankingApp/BankingApp/Scenes/Landing/LandingViewController.swift
@@ -0,0 +1,20 @@
+//
+//  LandingViewController.swift
+//  BankingApp
+//
+//  Created by Onur Torna on 30.11.2019.
+//  Copyright © 2019 Onur Torna. All rights reserved.
+//
+
+import UIKit
+
+class LandingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
